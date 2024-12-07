cfac20f07f25aa5d1b4587373ccdf628e0e5a237
Vitor <vitorloula3@gmail.com>
2024-12-06 21:59:37 -0300

    feat: Alteração para que a top app bar funcione de maneira dinamica

--- app/src/main/java/com/connectdeaf/ui/components/TopAppBarConnectDeaf.kt
+++ app/src/main/java/com/connectdeaf/ui/components/TopAppBarConnectDeaf.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -24,6 +25,7 @@ import com.connectdeaf.R
 @Composable
 fun TopAppBar(
     navController: NavController? = null,
+    showBackButton: Boolean = false
 ) {
     CenterAlignedTopAppBar(
         title = {
@@ -38,29 +40,44 @@ fun TopAppBar(
             }
         },
         navigationIcon = {
-            IconButton(onClick = {
-                navController?.navigate("Notifications") {
-                    launchSingleTop = true
+            if (showBackButton) {
+
+                IconButton(onClick = { navController?.popBackStack() }) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = "Voltar",
+                        tint = Color.White
+                    )
                 }
-            }) {
-                Icon(
-                    imageVector = Icons.Default.Notifications,
-                    contentDescription = "Notificação",
-                    tint = Color.White
-                )
+            } else {
+
+                IconButton(onClick = {
+                    navController?.navigate("Notifications") {
+                        launchSingleTop = true
+                    }
+                }) {
+                    Icon(
+                        imageVector = Icons.Default.Notifications,
+                        contentDescription = "Notificação",
+                        tint = Color.White
+                    )
+                }
             }
         },
         actions = {
-            IconButton(onClick = {
-                navController?.navigate("Menu") {
-                    launchSingleTop = true
+
+            if (!showBackButton) {
+                IconButton(onClick = {
+                    navController?.navigate("Menu") {
+                        launchSingleTop = true
+                    }
+                }) {
+                    Icon(
+                        imageVector = Icons.Default.Menu,
+                        contentDescription = "Menu",
+                        tint = Color.White
+                    )
                 }
-            }) {
-                Icon(
-                    imageVector = Icons.Default.Menu,
-                    contentDescription = "Menu",
-                    tint = Color.White
-                )
             }
         },
         colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
