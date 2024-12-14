1c64e6423fada249a6e968ae1ce63c83430aa726
Elenildo Filho <elenildovas123@gmail.com>
2024-12-14 12:20:44 -0300

    feat: adicionando DrawerMenu na HomeScreen

--- app/src/main/java/com/connectdeaf/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/connectdeaf/ui/screens/HomeScreen.kt
@@ -2,11 +2,13 @@ package com.connectdeaf.ui.screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -23,82 +25,103 @@ import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.connectdeaf.R
-import com.connectdeaf.ui.components.ChipComponent
+import com.connectdeaf.ui.components.DrawerMenu
 import com.connectdeaf.ui.components.SearchBarField
 import com.connectdeaf.ui.theme.PrimaryColor
+import com.connectdeaf.ui.theme.TextColorPrimary
+import kotlinx.coroutines.launch
 
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen( navController: NavHostController) {
 
     var searchQuery by remember { mutableStateOf(TextFieldValue("")) }
 
-    Scaffold(
-        topBar = {
-            com.connectdeaf.ui.components.TopAppBar(navController = null, showBackButton = false)
-        }
-    ) { paddingValues ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(paddingValues)
-                .padding(24.dp),
-            verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(
-                text = buildAnnotatedString {
-                    append("Encontre o profissional ")
-                    withStyle(
-                        style = SpanStyle(
-                            color = PrimaryColor,
-                        )
-                    ) {
-                        append("capacitado ")
-                    }
-                    append("para o seu serviço ")
-                    withStyle(
-                        style = SpanStyle(
-                            color = PrimaryColor,
-                        )
-                    ) {
-                        append("imediatamente.")
-                    }
-                },
-                style = TextStyle(
-                    fontSize = 20.sp,
-                    fontWeight = FontWeight.Medium,
-                    color = Color.Black,
-                    textAlign = TextAlign.Center
-                ),
-                modifier = Modifier.padding(top = 16.dp),
-            )
+    val drawerStateMenu = rememberDrawerState(DrawerValue.Closed)
+    val drawerStateNotifications = rememberDrawerState(DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
 
-            Spacer(modifier = Modifier.height(24.dp))
-
-            SearchBarField(
-                onSearchQueryChange = { searchQuery = it },
-                placeholder = "Pesquisar por serviço...",
-                searchQuery = searchQuery
-            )
-
-            Spacer(modifier = Modifier.height(16.dp))
-
-            TagsSection(tags = listOf("Designer", "IA", "Outra", "Tag", "Desenvolvimento"))
-
-            Spacer(modifier = Modifier.weight(1f))
-
-            Image(
-                painter = painterResource(id = R.drawable.home_image),
-                contentDescription = "Ilustração Home",
+    DrawerMenu(
+        navController = navController,
+        scope = scope,
+        drawerStateMenu = drawerStateMenu,
+        drawerStateNotifications = drawerStateNotifications
+    ) {
+        Scaffold(
+            topBar = {
+                com.connectdeaf.ui.components.TopAppBar(
+                    onOpenDrawerMenu = { scope.launch { drawerStateMenu.open() } },
+                    onOpenDrawerNotifications = { scope.launch { drawerStateNotifications.open() } },
+                    showBackButton = false
+                )
+            }
+        ) { paddingValues ->
+            Column(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 40.dp),
-                contentScale = ContentScale.Crop,
-            )
+                    .fillMaxSize()
+                    .padding(paddingValues)
+                    .padding(24.dp),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = buildAnnotatedString {
+                        append("Encontre o profissional ")
+                        withStyle(
+                            style = SpanStyle(
+                                color = PrimaryColor,
+                            )
+                        ) {
+                            append("capacitado ")
+                        }
+                        append("para o seu serviço ")
+                        withStyle(
+                            style = SpanStyle(
+                                color = PrimaryColor,
+                            )
+                        ) {
+                            append("imediatamente.")
+                        }
+                    },
+                    style = TextStyle(
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Medium,
+                        color = Color.Black,
+                        textAlign = TextAlign.Center
+                    ),
+                    modifier = Modifier.padding(top = 16.dp),
+                )
+
+                Spacer(modifier = Modifier.height(24.dp))
+
+                SearchBarField(
+                    onSearchQueryChange = { searchQuery = it },
+                    placeholder = "Pesquisar por serviço...",
+                    searchQuery = searchQuery
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                TagsSection(tags = listOf("Designer", "IA", "Outra", "Tag", "Desenvolvimento"))
+
+                Spacer(modifier = Modifier.weight(1f))
+
+                Image(
+                    painter = painterResource(id = R.drawable.home_image),
+                    contentDescription = "Ilustração Home",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 40.dp),
+                    contentScale = ContentScale.Crop,
+                )
+            }
         }
     }
+
+
 }
 
 @OptIn(ExperimentalLayoutApi::class)
@@ -124,14 +147,29 @@ fun TagsSection(tags: List<String>) {
             horizontalArrangement = Arrangement.spacedBy(8.dp)
         ) {
             tags.forEach { tag ->
-                ChipComponent(tag)
+                HomeChip(tag)
             }
         }
     }
 }
 
+@Composable
+fun HomeChip(text: String) {
+    ElevatedAssistChip(
+        onClick = { /* TODO: Tag Action */ },
+        shape = RoundedCornerShape(16.dp),
+        label = { Text(text = text, style = TextStyle(fontSize = 14.sp)) },
+        colors = AssistChipDefaults.assistChipColors(
+            containerColor = Color(0xFFE2E8F7),
+            labelColor = TextColorPrimary
+        )
+    )
+}
+
 @Preview(showBackground = true)
 @Composable
 fun HomeScreenPreview() {
-    HomeScreen()
+    HomeScreen(
+        navController = rememberNavController()
+    )
 }
