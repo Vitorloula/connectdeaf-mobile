08d6e3f1220607952b5ed2987f6698662a5f3dc4
kairosb <kairomatheussb@gmail.com>
2025-01-14 02:45:51 -0300

    feat: ajustando Retrofit

--- app/src/main/java/com/connectdeaf/network/RetrofitInstance.kt
+++ app/src/main/java/com/connectdeaf/network/RetrofitInstance.kt
@@ -1,16 +1,32 @@
 package com.connectdeaf.network
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
+
     private const val BASE_URL = "https://connectdeaf-dev.azurewebsites.net/"
 
-    val api: ApiService by lazy {
-        Retrofit.Builder()
+    fun api(getToken: (() -> String?)? = null): ApiService {
+        val clientBuilder = OkHttpClient.Builder()
+
+        if (getToken != null) {
+            clientBuilder.addInterceptor(AuthInterceptor(getToken))
+        }
+
+        clientBuilder.addInterceptor(HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        })
+
+        val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(clientBuilder.build())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-            .create(ApiService::class.java)
+
+        return retrofit.create(ApiService::class.java)
     }
+
 }
