4b560ea8e561f413f7cf51d1d02e4a46f749313b
kairosb <kairomatheussb@gmail.com>
2025-01-23 16:51:37 -0300

    feat: ajustando interceptor no caso de login

--- app/src/main/java/com/connectdeaf/network/interceptors/AuthInterceptor.kt
+++ app/src/main/java/com/connectdeaf/network/interceptors/AuthInterceptor.kt
@@ -5,6 +5,11 @@ import okhttp3.Response
 
 class AuthInterceptor(private val getToken: () -> String?) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+        if (request.url.encodedPath.contains("/api/auth/login")) {
+            return chain.proceed(request)
+        }
+
         val token = getToken()
         val requestBuilder = chain.request().newBuilder()
 
