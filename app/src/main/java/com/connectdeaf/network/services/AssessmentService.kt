05c9fa2e35a494831bad93c7ebc34bd0e6722c10
kairosb <kairomatheussb@gmail.com>
2025-01-27 00:13:52 -0300

    feat: adicionando rota

--- app/src/main/java/com/connectdeaf/network/services/AssessmentService.kt
+++ app/src/main/java/com/connectdeaf/network/services/AssessmentService.kt
@@ -1,10 +1,16 @@
 package com.connectdeaf.network.services
 
 import com.connectdeaf.domain.model.Assessment
+import com.connectdeaf.network.dtos.AssessmentRequest
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface AssessmentService {
     @GET("api/comment/service/{service_id}")
     suspend fun getAssessmentByService(@Path("service_id") id: String): List<Assessment>
+
+    @POST("api/comment")
+    suspend fun createAssessment(@Body assessment: AssessmentRequest): Assessment
 }
