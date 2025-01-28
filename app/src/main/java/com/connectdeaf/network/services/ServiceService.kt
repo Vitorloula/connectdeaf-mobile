eb11b74215564aede126279d521b9dc66845b296
Vitor <vitorloula3@gmail.com>
2025-01-27 22:45:45 -0300

    feat: implementação dos metodos post e Delete

--- app/src/main/java/com/connectdeaf/network/services/ServiceService.kt
+++ app/src/main/java/com/connectdeaf/network/services/ServiceService.kt
@@ -1,8 +1,14 @@
 package com.connectdeaf.network.services
 
 import com.connectdeaf.domain.model.Service
+import com.connectdeaf.network.dtos.ServiceRequest
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface ServiceService {
     @GET("api/services/professional/{professional_id}/services")
@@ -10,6 +16,16 @@ interface ServiceService {
 
     @GET("api/services")
     suspend fun getServices(): List<Service>
+
+    @POST("api/services")
+    suspend fun createService(
+        @Query("professionalId") professionalId: String,
+        @Body serviceRequest: ServiceRequest
+    ): Service
+
+    @DELETE("api/services/{service_id}")
+    suspend fun deleteService(@Path("service_id") serviced: String): Response<Unit>
+
 
     @GET("api/services/{service_id}")
     suspend fun getService(@Path("service_id") id: String): Service
