48c283d0aa6ea8e2038d9dbeb09bd62e91b4613b
Fernando Lima <eufernandoprogramador@gmail.com>
2025-01-27 23:20:31 -0300

    feat: adicionando dados de horário do profissional

--- app/src/main/java/com/connectdeaf/network/dtos/ProfessionalRequest.kt
+++ app/src/main/java/com/connectdeaf/network/dtos/ProfessionalRequest.kt
@@ -1,5 +1,8 @@
 package com.connectdeaf.network.dtos
 
+import java.time.Duration
+import java.time.LocalTime
+
 data class ProfessionalRequest(
     val name: String,
     val email: String,
@@ -7,9 +10,12 @@ data class ProfessionalRequest(
     val phoneNumber: String,
     val areaOfExpertise: String,
     val qualification: String,
+    val workStartTime: String,
+    val workEndTime: String,
+    val breakDuration: String,
     val addresses: List<AddressRequest>
 ) {
     override fun toString(): String {
-        return "ProfessionalRequest(name='$name', email='$email', password='*****', phoneNumber='$phoneNumber', areaOfExpertise='$areaOfExpertise', qualification='$qualification', addresses=$addresses)"
+        return "ProfessionalRequest(name='$name', email='$email', password='*****', phoneNumber='$phoneNumber', areaOfExpertise='$areaOfExpertise', qualification='$qualification', workStartTime=$workStartTime, workEndTime=$workEndTime, addresses=$addresses)"
     }
 }
