54194ee174dbbbe8f6ce59c2c67b1b1946e62697
Vitor <vitorloula3@gmail.com>
2025-02-16 18:47:03 -0300

    feat: corrigindo tela de serviço e colocando o fluxo de agendamento certo

--- app/src/main/java/com/connectdeaf/ui/screens/ServiceScreen.kt
+++ app/src/main/java/com/connectdeaf/ui/screens/ServiceScreen.kt
@@ -1,43 +1,30 @@
 package com.connectdeaf.ui.screens
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.*
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import coil.compose.rememberAsyncImagePainter
 import com.connectdeaf.R
-import com.connectdeaf.viewmodel.DrawerViewModel
-import kotlinx.coroutines.launch
-
-import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.sp
 import com.connectdeaf.ui.components.AssessmentCard
+import com.connectdeaf.viewmodel.DrawerViewModel
 import com.connectdeaf.viewmodel.ServiceViewModel
+import kotlinx.coroutines.launch
 
 @Composable
 fun ServiceScreen(
@@ -50,7 +37,7 @@ fun ServiceScreen(
     val assessmentState = viewModel.serviceAssessment.collectAsState()
     val scope = rememberCoroutineScope()
     val context = LocalContext.current
-    val category: List<String> = listOf("Categoria 1", "Categoria 2")
+    val category = listOf("Categoria 1", "Categoria 2")
 
     LaunchedEffect(serviceId) {
         viewModel.fetchServiceDetail(serviceId, context)
@@ -76,29 +63,27 @@ fun ServiceScreen(
                     .verticalScroll(rememberScrollState()),
                 verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
-                // Card do Perfil
                 CardProfile(
                     name = service.professional.name,
                     city = service.professional.addresses[0].city,
                     state = service.professional.addresses[0].state,
                     description = "Eu sou um profissional muito bom",
-                    imageUrl = null,
-                    professionalId = service.professional.id,
-                    serviceId = service.id,
-                    navController = navController
+                    imageUrl = null
                 )
 
-                // Detalhes do Serviço
                 CardDetailService(
                     title = service.name,
                     description = service.description,
-                    category = category
+                    category = category,
+                    value = service.value.toString(),
+                    navController = navController,
+                    professionalId = service.professional.id,
+                    serviceId = service.id
                 )
 
                 SectionCardService(title = "Avaliações sobre o serviço") {
                     Row(
-                        modifier = Modifier
-                            .horizontalScroll(rememberScrollState()),
+                        modifier = Modifier.horizontalScroll(rememberScrollState()),
                         horizontalArrangement = Arrangement.spacedBy(12.dp)
                     ) {
                         assessmentState.value?.forEach { assessment ->
@@ -109,92 +94,53 @@ fun ServiceScreen(
                             )
                         }
                     }
-
                 }
             }
-        } ?: run {
-            CircularProgressIndicator()
-        }
+        } ?: CircularProgressIndicator()
     }
 }
 
 @Composable
 fun CardProfile(
-    name: String,
-    city: String,
-    state: String,
-    description: String,
-    imageUrl: String?,
-    professionalId: String,
-    serviceId: String,
-    navController: NavController
+    name: String, city: String, state: String, description: String,
+    imageUrl: String?
 ) {
     Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp),
+        modifier = Modifier.fillMaxWidth().padding(8.dp),
         shape = RoundedCornerShape(12.dp),
         elevation = CardDefaults.cardElevation(4.dp),
         colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.background)
     ) {
         Row(
-            modifier = Modifier
-                .padding(16.dp)
-                .fillMaxWidth(),
+            modifier = Modifier.padding(16.dp).fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
-            // Avatar
             Image(
                 painter = if (imageUrl.isNullOrEmpty()) painterResource(id = R.drawable.ic_user_placeholder)
                 else rememberAsyncImagePainter(model = imageUrl),
                 contentDescription = "Avatar",
-                modifier = Modifier
-                    .size(64.dp)
-                    .background(Color.Gray, shape = CircleShape)
-                    .padding(8.dp)
+                modifier = Modifier.size(64.dp).background(Color.Gray, CircleShape).padding(8.dp)
             )
 
-            // Informações do perfil
             Column(
-                modifier = Modifier
-                    .padding(start = 16.dp)
-                    .weight(1f),
+                modifier = Modifier.padding(start = 16.dp).weight(1f),
                 verticalArrangement = Arrangement.spacedBy(8.dp),
             ) {
-                Text(
-                    text = name,
-                    style = MaterialTheme.typography.titleLarge,
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis
-                )
-                Text(
-                    text = "$city - $state",
-                    style = MaterialTheme.typography.bodyMedium,
-                    color = MaterialTheme.colorScheme.primary
-                )
-                Text(
-                    text = description,
-                    style = MaterialTheme.typography.bodySmall
-                )
+                Text(text = name, style = MaterialTheme.typography.titleLarge, maxLines = 1, overflow = TextOverflow.Ellipsis)
+                Text(text = "$city - $state", style = MaterialTheme.typography.bodyMedium, color = MaterialTheme.colorScheme.primary)
+                Text(text = description, style = MaterialTheme.typography.bodySmall)
             }
 
-            // Botões
-            Column(
-                verticalArrangement = Arrangement.spacedBy(8.dp)
-            ) {
-                Button(onClick = { /* Lógica para agendar serviço */ }) {
-                    Icon(Icons.Default.DateRange, contentDescription = null)
-                    Spacer(modifier = Modifier.width(8.dp))
-                    Text(text = "Agendar Serviço")
-                }
-            }
         }
     }
 }
 
 @Composable
-fun CardDetailService(title: String, description: String, category: List<String>) {
+fun CardDetailService(title: String, description: String, category: List<String>,
+                      value: String,
+                      navController: NavController,
+                      professionalId: String, serviceId: String) {
     Card(
         modifier = Modifier.fillMaxWidth(),
         shape = RoundedCornerShape(12.dp),
@@ -205,45 +151,34 @@ fun CardDetailService(title: String, description: String, category: List<String>
             modifier = Modifier.padding(16.dp),
             verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
-            // Imagem do Serviço
-            Box(
-                modifier = Modifier
-                    .height(200.dp)
-                    .fillMaxWidth()
-            ) {
-                val imagePainter = rememberAsyncImagePainter("https://picsum.photos/800/600")
-                Image(
-                    painter = imagePainter,
-                    contentDescription = "Service Image",
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier.fillMaxSize()
-                )
+            Image(
+                painter = rememberAsyncImagePainter("https://picsum.photos/800/600"),
+                contentDescription = "Service Image",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxWidth().height(200.dp)
+            )
+
+            Text(text = title, style = MaterialTheme.typography.titleMedium)
+            Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+                category.forEach { ChipService(label = it) }
             }
 
-            // Título e Detalhes do Serviço
-            Text(
-                text = title,
-                style = MaterialTheme.typography.titleMedium
-            )
-            if (category.isNotEmpty()) {
-                Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
-                    category.forEach { cat ->
-                        ChipService(label = cat)
-                    }
-                }
+            Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
+                Text(text = "Sobre esse serviço:", style = MaterialTheme.typography.bodyLarge)
+                Text(text = description, style = MaterialTheme.typography.bodyMedium)
             }
-            Column(
-                verticalArrangement = Arrangement.spacedBy(8.dp),
-                modifier = Modifier.padding(top = 8.dp)
-            ) {
-                Text(
-                    text = "Sobre esse serviço:",
-                    style = MaterialTheme.typography.bodyLarge
-                )
-                Text(
-                    text = description,
-                    style = MaterialTheme.typography.bodyMedium
-                )
+
+            Row(
+                modifier = Modifier.fillMaxWidth().padding(top = 8.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            )
+
+            {
+                Button(onClick = { navController.navigate("appointmentScreen/${serviceId}/${professionalId}/${value}") }) {
+                    Icon(Icons.Default.DateRange, contentDescription = null)
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text(text = "Agendar")
+                }
             }
         }
     }
@@ -257,13 +192,8 @@ fun SectionCardService(title: String, content: @Composable () -> Unit) {
         elevation = CardDefaults.cardElevation(4.dp),
         colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.background)
     ) {
-        Column(
-            modifier = Modifier.padding(16.dp)
-        ) {
-            Text(
-                text = title,
-                style = MaterialTheme.typography.titleMedium
-            )
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(text = title, style = MaterialTheme.typography.titleMedium)
             content()
         }
     }
@@ -272,16 +202,8 @@ fun SectionCardService(title: String, content: @Composable () -> Unit) {
 @Composable
 fun ChipService(label: String) {
     Box(
-        modifier = Modifier
-            .background(MaterialTheme.colorScheme.primary, shape = RoundedCornerShape(50))
-            .padding(horizontal = 8.dp, vertical = 4.dp)
+        modifier = Modifier.background(MaterialTheme.colorScheme.primary, RoundedCornerShape(50)).padding(8.dp)
     ) {
-        Text(
-            text = label,
-            color = Color.White,
-            style = MaterialTheme.typography.bodySmall
-        )
+        Text(text = label, color = Color.White, style = MaterialTheme.typography.bodySmall)
     }
 }
-
-
