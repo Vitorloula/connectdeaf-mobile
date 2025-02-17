4930e897ee93fd3a21f40d7765daa6df13186f81
Elenildo Filho <elenildovas123@gmail.com>
2025-02-16 23:13:08 -0300

    feat: adicionando notificacao a lista

--- app/src/main/java/com/connectdeaf/ui/components/NotificationDrawerContent.kt
+++ app/src/main/java/com/connectdeaf/ui/components/NotificationDrawerContent.kt
@@ -1,5 +1,6 @@
 package com.connectdeaf.ui.components
 
+import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -19,15 +20,20 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.connectdeaf.data.model.notifications
+import com.connectdeaf.viewmodel.NotificationViewModel
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @Composable
 fun NotificationDrawerContent(
     navController: NavController,
     drawerState: DrawerState,
-    scope: CoroutineScope
+    scope: CoroutineScope,
+    notificationViewModel: NotificationViewModel? = null
 ) {
+
+    val notifications = notificationViewModel?.getNotifications()
+
     Surface(
         modifier = Modifier
             .fillMaxHeight()
@@ -49,16 +55,34 @@ fun NotificationDrawerContent(
                 modifier = Modifier.padding(vertical = 8.dp)
             )
 
-            LazyColumn(modifier = Modifier.fillMaxWidth()) {
-                items(notifications) { notification ->
-                    NotificationCard(
-                        notification = notification,
-                        modifier = Modifier.fillMaxWidth(),
-                        onClick = {
-                            println("Notification clicked: ${notification.title}")
+            if (notifications != null) {
+                if (notifications.isNotEmpty()) {
+
+                    Log.d("not", notifications.toString())
+
+                    LazyColumn(modifier = Modifier.fillMaxWidth()) {
+                        items(notifications) { notification ->
+                            NotificationCard(
+                                notification = notification,
+                                modifier = Modifier.fillMaxWidth(),
+                                onClick = {
+                                    navController.navigate("ScheduleScreen")
+                                    scope.launch { drawerState.close() }
+                                }
+                            )
                         }
+                    }
+                } else {
+                    Text(
+                        text = "Nenhuma notificação encontrada.",
+                        modifier = Modifier.padding(vertical = 16.dp)
                     )
                 }
+            } else {
+                Text(
+                    text = "ViewModel Null.",
+                    modifier = Modifier.padding(vertical = 16.dp)
+                )
             }
         }
     }
