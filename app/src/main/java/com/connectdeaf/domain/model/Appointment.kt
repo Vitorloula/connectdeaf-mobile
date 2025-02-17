8264275f786d7147e70198e77f259eec0a58579e
Kairo Sales <kairomatheussb@gmail.com>
2025-02-17 01:04:14 -0300

    feat: adicionando model

--- app/src/main/java/com/connectdeaf/domain/model/Appointment.kt
+++ app/src/main/java/com/connectdeaf/domain/model/Appointment.kt
@@ -1,14 +1,22 @@
 package com.connectdeaf.viewmodel
 
 
+import com.connectdeaf.domain.model.Service
 import java.util.UUID
 
+data class Schedule(
+    val id: UUID,
+    val date: String,
+    val startTime: String,
+    val endTime: String,
+    val professionalId: UUID
+)
 
 data class Appointment(
     val id: UUID? = null,             // ID único
     val customerId: UUID,             // ID do cliente associado
     val professionalId: UUID,         // ID do profissional associado
-    val serviceId: UUID,              // ID do serviço associado
-    val scheduleId: UUID? = null,     // ID do horário (opcional)
-    val status: String                // Status do agendamento (ex.: "Pendente", "Aprovada")
+    val service: Service,              // ID do serviço associado
+    val schedule: Schedule,     // ID do horário (opcional)
+    val status:   String                // Status do agendamento (ex.: "Pendente", "Aprovada")
 )
