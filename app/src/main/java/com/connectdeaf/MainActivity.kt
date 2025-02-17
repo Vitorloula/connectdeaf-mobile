2abdc865b3ddcd1cda5b40581e0b82245e1f36de
Elenildo Filho <elenildovas123@gmail.com>
2025-02-16 23:07:51 -0300

    cleanup: removendo imports não utilizados

--- app/src/main/java/com/connectdeaf/MainActivity.kt
+++ app/src/main/java/com/connectdeaf/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.connectdeaf
 
-import android.app.NotificationChannel
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,15 +8,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
-import com.connectdeaf.di.appModule
 import com.connectdeaf.navigation.AppNavigation
 import com.connectdeaf.ui.screens.RegisterScreen
 import com.connectdeaf.ui.theme.ConnectDeafTheme
-import com.connectdeaf.utils.createNotificationChannel
 import com.connectdeaf.utils.initializeNotifications
-import com.connectdeaf.utils.requestNotificationPermission
-import org.koin.android.ext.koin.androidContext
-import org.koin.core.context.GlobalContext.startKoin
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
