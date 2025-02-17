917bcf1051afbafb482985e5f037b33945b1284e
Kairo Sales <kairomatheussb@gmail.com>
2025-02-17 01:04:01 -0300

    feat: adicionando enpoint

--- app/src/main/java/com/connectdeaf/network/services/AppointmentService.kt
+++ app/src/main/java/com/connectdeaf/network/services/AppointmentService.kt
@@ -1,8 +1,11 @@
 package com.connectdeaf.network.services
 
 import com.connectdeaf.network.dtos.AppointmentRequest
+import com.connectdeaf.viewmodel.Appointment
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface AppointmentService {
@@ -13,4 +16,8 @@ interface AppointmentService {
         @Query("serviceId") serviceId: String,
         @Body appointmentRequest: AppointmentRequest
     )
+
+    @GET("api/appointments/professional/{professionalId}")
+    suspend fun getAppointmentsByProfessionalId(@Path("professionalId") userId: String): List<Appointment>
+
 }
