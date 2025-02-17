ccd37f1e2e9bbd9c190bd54ba952ab39b8acd013
Vitor <vitorloula3@gmail.com>
2025-02-17 18:03:47 -0300

    feat: ajeitando passagem de parametro AppointmentRequest

--- app/src/main/java/com/connectdeaf/network/dtos/AppointmentRequest.kt
+++ app/src/main/java/com/connectdeaf/network/dtos/AppointmentRequest.kt
@@ -1,4 +1,4 @@
 package com.connectdeaf.network.dtos
 
 data class TimeSlotRequest(val startTime: String, val endTime: String)
-data class AppointmentRequest(val date: String, val time: String)
+data class AppointmentRequest(val customerId: String, val professionalId: String, val serviceId: String, val date: String, val startTime: String, val endTime: String)
