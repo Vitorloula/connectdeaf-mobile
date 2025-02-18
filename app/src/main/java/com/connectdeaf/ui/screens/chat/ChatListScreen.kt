5aeb3117960c2f1b843c50e9e278e746182b79d0
Fernando Lima <eufernandoprogramador@gmail.com>
2025-02-17 23:01:01 -0300

    fix: corrigindo bug do viewmodel

--- app/src/main/java/com/connectdeaf/ui/screens/chat/ChatListScreen.kt
+++ app/src/main/java/com/connectdeaf/ui/screens/chat/ChatListScreen.kt
@@ -196,7 +196,6 @@ fun ChatListScreen(
     DrawerMenu(
         navController = navController,
         scope = scope,
-        gesturesEnabled = false,
         drawerViewModel = drawerViewModel
     ) {
         Scaffold(
